ab9d3d3e250bf43e5ddf017e254ab78d1d2be988
Nikhil <nikhilpatel.indore@gmail.com>
2020-04-26 18:59:02 +0530

    apis functional without country data merge

--- routes/jhu-data.js
+++ routes/jhu-data.js
@@ -3,9 +3,14 @@ const router = express.Router()
 const csvtojson = require('csvtojson')
 const request = require('request')
 const lookup = require('country-code-lookup')
+const nestedProperty = require('nested-property')
+const cron = require('node-cron');
+
 
 var data = {
-  brief : {},
+  total : {},
+  current: {},
+  time_series: {}
 }
 
 const global_stats_source = {
@@ -26,16 +31,30 @@ const correctedCountryNames = {
   "Congo (Kinshasa)": "Republic of the Congo"
 }
 
+//
+// cron.schedule('1 * * * *', () => {
+//   console.log('Updating data'); //each hour
+//   // updateData()
+// });
+
 let lastUpdated;
 
-router.get('/brief',(req,res) => {
-
-})
-
-router.post('/get',async (req,res)=>{
+router.get('/total',async (req,res) => {
   var ans = await updateData()
 
-  res.send(ans)
+  res.json(data.total)
+})
+
+router.get('/current',async (req,res)=>{
+  var ans = await updateData()
+
+  res.json(data.current)
+})
+
+router.get('/time_series',async (req,res)=>{
+  var ans = await updateData()
+
+  res.json(data.time_series)
 })
 
 async function updateData(){
@@ -59,8 +78,42 @@ async function updateData(){
   }
 
   const current = {}
+  //Rough structure
+  // current = {
+  //   name : {
+  //     country_region:
+  //     province_state:
+  //     countryCode:
+  //     location: {
+  //       lat:
+  //       long:
+  //     }
+  //     confirmed:
+  //     deaths:
+  //     recovered:
+  //   }
+  // }
 
   const time_series = {}
+  // rough structure
+  // time_series = {
+  //   name: {
+  //     country_region:
+  //     province_state:
+  //     countryCode:
+  //     location: {
+  //       lat:
+  //       long:
+  //     }
+  //     time_series: {
+  //       date: {
+  //         confirmed:
+  //         deaths:
+  //         recovered:
+  //       }
+  //     }
+  //   }
+  // }
 
   //Object.entries creates nested array from object
   for(const [type, obj] of Object.entries(rawData)){
@@ -78,13 +131,22 @@ async function updateData(){
       defineStructure(current, name, data)
       current[name][type] = latest
 
+      defineStructure(time_series, name, data)
+      var dates = keys.slice(4) //removng every key except dates
 
-
+      for (const date of dates){
+        var formatted_date = date.replace('/20','/2020')
+        formatted_date = formatted_date.replace('/2020/20','/20/2020')
+        nestedProperty.set(time_series[name], `time_series.${formatted_date}.${type}`, Number(data[date]))
+      }
     }
   }
 
-  return current;
+  data.total = total
+  data.current = current
+  data.time_series = time_series
 
+  return total
 }
 
 function getCSV_to_JSON(store,key){
@@ -109,11 +171,9 @@ function getCSV_to_JSON(store,key){
 
 function defineStructure(store,name,data){
   if(!store.hasOwnProperty(name)){
-    store[name] = {}
-    store[name]['Province/State'] = data['Province/State']
-    store[name]['Country/Region'] = data['Country/Region']
-    store[name].lastUpdated = lastUpdated
     store[name] = {
+      province_state : data['Province/State'],
+      country_region : data['Country/Region'],
       location: {
         lat: Number(data.Lat),
         lng: Number(data.Long)
